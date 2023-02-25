cdb12bef40262abcac9ca3db175b4cb5cea3b0fa
Hoenhime999 <111585154+Hoenhime999@users.noreply.github.com>
2023-02-25 20:16:45 +0530

    POLYNOMIALS(FINDING ZERO AND PLOTING GRAPH)
    dont mind the graph its weird

---
+++ polynomials.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+print("Please type the cofficients in the order x^n,x^n-1 up to x^0. Type done when cofficient of x^0 is typed")
+colist = []
+
+
+while True:
+    co = input('Cofficient: ')
+    try:
+        colist.append(int(co))
+    except ValueError:
+        if co == 'done':
+            break
+        else:
+            print('an error has occured ')
+    
+
+def y_value(x,coeff):
+    
+    coeff_deg = len(coeff)
+
+    ys = np.zeros(len(x))
+    for i in range(coeff_deg):
+        ys += coeff[i] * x**i
+    return ys
+
+roots = np.roots(colist)
+print(roots)
+colist.reverse()
+
+x = np.linspace(-100,100)
+
+
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.plot(x,y_value(x,colist))
+plt.show()
+
+
+
+
+
